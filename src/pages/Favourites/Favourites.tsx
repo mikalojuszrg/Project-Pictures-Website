fced3c2eadb63aba92412c44145cc88826443950
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-19 13:50:18 +0200

    commit

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,19 +1,50 @@
+import { useContext, useEffect, useRef, useState } from "react";
+
 import { FavouriteContext } from "../../contexts/FavouriteContext";
 import PhotoCard from "../../components/ImageCard/PhotoCard";
 import styles from "./Favourites.module.scss";
-import { useContext } from "react";
 
 const Favourites = () => {
   const { favouritePhotos } = useContext(FavouriteContext);
+  const [containerHeight, setContainerHeight] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      setContainerHeight(container.clientHeight);
+    }
+    const handleScroll = () => {
+      if (
+        container &&
+        container.scrollTop + container.clientHeight >=
+          container.scrollHeight - containerHeight &&
+        !loading
+      ) {
+        setLoading(true);
+        setLoading(false);
+      }
+    };
+    if (container) {
+      container.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [loading, containerRef, containerHeight]);
+
   return (
-    <main>
-      <div className={styles.grid}>
+    <section className={styles.container}>
+      <div className={styles.container__images} ref={containerRef}>
         {favouritePhotos.map((photo) => (
-          //   <img key={photo.id} src={photo.src.medium} alt={photo.photographer} />
           <PhotoCard {...photo} key={photo.id} />
         ))}
+        {/* {loading && <div>Loading...</div>} */}
       </div>
-    </main>
+    </section>
   );
 };
 
