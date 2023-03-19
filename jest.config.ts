7cf7efbdc07bca87214c8138e71121b9429051bd
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-19 14:06:12 +0200

    commit

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: ["<rootDir>/tests/**/*.test.(ts|tsx)"],
+};
+
+export default config;
